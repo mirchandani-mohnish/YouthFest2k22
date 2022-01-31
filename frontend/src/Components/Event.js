010a7946f0f3525390d63eb3c9feb902305eb47a
mirchandani-mohnish <mirchandani.mohnish03@gmail.com>
2022-01-31 15:31:24 +0530

    Card added - working Card onClick left to render

---
+++ frontend/src/Components/Event.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import {Card} from 'react-bootstrap'
+import { ListGroup,ListGroupItem } from "react-bootstrap";
+import './components.css';
+
+
+
+export default class event extends Component{
+    constructor(props){
+        super(props);
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                <div className="card 1">
+                  <div className="card_image"> <img src="https://i.redd.it/b3esnz5ra34y.jpg" /> </div>
+                  <div className="card_title title-white">
+                    <p>Card Title</p>
+                  </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+/*
+https://cdn.blackmilkclothing.com/media/wysiwyg/Wallpapers/PhoneWallpapers_FloralCoral.jpg
+https://media.giphy.com/media/10SvWCbt1ytWCc/giphy.gif
+https://media.giphy.com/media/LwIyvaNcnzsD6/giphy.gif
+
+*/
--- frontend/src/Components/Resume.js
+++ frontend/src/Components/Resume.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import Slide from "react-reveal";
+import Event from './Event';
+import {Container, Row, Col} from "react-bootstrap";
+import './components.css';
+
 
 class Resume extends Component {
   getRandomColor() {
@@ -60,19 +64,35 @@ class Resume extends Component {
           <div className="row education">
             <div className="three columns header-col">
               <h1>
-                <span>Education</span>
+                <span>Events</span>
               </h1>
+
             </div>
 
-            <div className="nine columns main-col">
-              <div className="row item">
+            <div className="eleven columns main-col">
+              {/* <div className="row item">
                 <div className="twelve columns">{education}</div>
+              </div> */}
+              <div className="cards-list">
+                <Event />
+                <Event />
               </div>
+
             </div>
           </div>
         </Slide>
 
-        <Slide left duration={1300}>
+        
+      </section>
+    );
+  }
+}
+
+export default Resume;
+
+
+
+/* <Slide left duration={1300}>
           <div className="row work">
             <div className="three columns header-col">
               <h1>
@@ -100,10 +120,4 @@ class Resume extends Component {
               </div>
             </div>
           </div>
-        </Slide>
-      </section>
-    );
-  }
-}
-
-export default Resume;
+        </Slide> */
