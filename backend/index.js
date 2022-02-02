9531238f966154e451cacbf3ef64d0f9219cd3a5
mirchandani-mohnish <mirchandani.mohnish03@gmail.com>
2022-02-02 10:17:30 +0530

    Access control origin error sorted

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000','http://youthfest.tk']
 }));
 
 const session1 = {
