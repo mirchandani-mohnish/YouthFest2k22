0126d13ec9cd35fc23968894b10e220bb042291d
Mohnish Mirchandani <mirchandani.mohnish03@gmail.com>
2022-06-18 06:16:00 +0530

    Patch: Logo error fixed

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 import Fade from "react-reveal";
-import './css/About.css';
-
+import "./css/About.css";
 
 class About extends Component {
   render() {
-    
-
-    
-
     return (
       <section id="about">
         <Fade duration={1000}>
@@ -25,12 +20,29 @@ class About extends Component {
               <h2>About YouthFest</h2>
 
               <p>
-              The Youth Fest is a vibrant platform for students of the University to showcase their talent, to sharpen their skills,  to creatively express themselves, to mingle, to collaborate and of course to build lifelong memories.
-              The Youth Fest is back with a bang this year, and we have some very  exciting news!<br />
-              We are delighted to announce the theme of Youth Fest 2022, which is: "India Recalled"
-              With this theme, we at Ahmedabad University wish to go back to our roots, our traditions and revisit our culture. There is a lot to learn from our own history; from ancient civilisations to post- independence era. The diversity in our culture gives us the opportunity to explore both; traditional and contemporary knowledge systems.
-              <br />Delving deep into our roots will allow us to reinvent and redefine what culture and history mean to us.We invite you on board to experience an exciting mix of events, competitions and performances!
-              <br /><b>Watch out for our emails and follow our Instagram for more updates!</b>
+                The Youth Fest is a vibrant platform for students of the
+                University to showcase their talent, to sharpen their skills, to
+                creatively express themselves, to mingle, to collaborate and of
+                course to build lifelong memories. The Youth Fest is back with a
+                bang this year, and we have some very exciting news!
+                <br />
+                We are delighted to announce the theme of Youth Fest 2022, which
+                is: "India Recalled" With this theme, we at Ahmedabad University
+                wish to go back to our roots, our traditions and revisit our
+                culture. There is a lot to learn from our own history; from
+                ancient civilisations to post- independence era. The diversity
+                in our culture gives us the opportunity to explore both;
+                traditional and contemporary knowledge systems.
+                <br />
+                Delving deep into our roots will allow us to reinvent and
+                redefine what culture and history mean to us.We invite you on
+                board to experience an exciting mix of events, competitions and
+                performances!
+                <br />
+                <b>
+                  Watch out for our emails and follow our Instagram for more
+                  updates!
+                </b>
               </p>
               <div className="row">
                 {/* <div className="columns contact-details">
