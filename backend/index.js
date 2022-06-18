99fd7ec049c0febe4994075b9a25bb5094ec3d30
Mohnish Mirchandani <mirchandani.mohnish03@gmail.com>
2022-06-18 06:09:47 +0530

    Update: Minor UI/UX changes and Bug Fixes

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
-const {verify} = require('./middleware')
-const cookieparser = require('cookie-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+const { verify } = require("./middleware");
+const cookieparser = require("cookie-parser");
 
-const cookieSession = require('cookie-session');
-require('dotenv').config({path: __dirname + '/.env'});
+const cookieSession = require("cookie-session");
+require("dotenv").config({ path: __dirname + "/.env" });
 
-
-// app initialization and middleware 
+// app initialization and middleware
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors({
-  origin: ['http://localhost:3000','http://youthfest.tk']
-}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://youthfest.tk",
+      "http://157.245.98.117:8080",
+    ],
+  })
+);
 app.use(cookieparser());
 
 const session1 = {
@@ -26,17 +31,15 @@ const session1 = {
   saveUninitialized: true,
 };
 
-
 app.use(cookieSession(session1));
 
-app.set('view engine','ejs');
-app.set('views','./templates');
-
+app.set("view engine", "ejs");
+app.set("views", "./templates");
 
 const eventsR = require("./routes/events");
 const adminR = require("./routes/admin");
-app.use('/events', eventsR);
-app.use('/admin', adminR);
+app.use("/events", eventsR);
+app.use("/admin", adminR);
 
 //const userR = require("./routes/user");
 //const logR = require("./routes/logInOut");
@@ -47,25 +50,21 @@ app.use('/admin', adminR);
 //app.use('/user', userR);
 //app.use('/log', logR);
 
-
-
 // Database and Port setup
 const CONN_URL = `mongodb+srv://${process.env.DB_UNAME}:${process.env.DB_PWD}@cluster0.elyih.mongodb.net/yFEST?retryWrites=true&w=majority`;
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-mongoose.connect(CONN_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {app.listen(PORT); console.log("server working");}).catch((e) => console.log(e.message));
-
-
-
-
-
-
+mongoose
+  .connect(CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT);
+    console.log("server working");
+  })
+  .catch((e) => console.log(e.message));
 
 // app.use(passport.initialize());
 // app.use(passport.session());
-
-
 
 // // routes
 // require('./routes/passport-setup.js');
@@ -88,12 +87,9 @@ mongoose.connect(CONN_URL, {useNewUrlParser: true, useUnifiedTopology: true}).th
 //   res.send("Login Success");
 // })
 
-
 // const userR = require("./routes/user");
 // const logR = require("./routes/logInOut");
 // // app.use('/clubs', clubs);
 // // app.use('/events', events);
 // app.use('/user', userR);
 // app.use('/log', logR);
-
-
--- frontend/src/Components/Resume.js
+++ frontend/src/Components/Resume.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import Slide from "react-reveal";
-import Event from './Event';
+import Event from "./Event";
 
-import './components.css';
-import './css/searchbar.css';
-import Modal from './EventDesc';
-import Axios from 'axios';
-
-
+import "./components.css";
+import "./css/searchbar.css";
+import Modal from "./EventDesc";
+import Axios from "axios";
 
 class Resume extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       EventArray: [],
       EventPg: false,
       CurrentEventData: "",
-      searchVal:"",
-    }
+      searchVal: "",
+    };
 
     this.searchBoxRef = React.createRef();
 
@@ -25,116 +23,105 @@ class Resume extends Component {
     this.handleClose = this.handleClose.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
   }
-  
-  async componentDidMount(){
-      try{
-          const eventList = await Axios.get('http://youthfest.tk:8000/events');
-          
 
-          this.setState({
-              EventArray: eventList.data
-          })
-      }catch(err){
-          console.log(err);
-          
-      }
+  async componentDidMount() {
+    try {
+      const eventList = await Axios.get("http://157.245.98.117:8000/events");
 
+      this.setState({
+        EventArray: eventList.data,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  handleClick(currEvent){
+  handleClick(currEvent) {
     this.setState({
       EventPg: true,
-      CurrentEventData: currEvent
-    })
+      CurrentEventData: currEvent,
+    });
     console.log("function called", this.state.EventPg);
-    
   }
-  handleClose(){
+  handleClose() {
     this.setState({
       EventPg: false,
-      CurrentEventData: ""
-    })
+      CurrentEventData: "",
+    });
   }
 
-  handleSearch(){
+  handleSearch() {
     this.setState({
-        searchVal: this.searchBoxRef.current.value
-    })
-    
+      searchVal: this.searchBoxRef.current.value,
+    });
   }
 
-
-  renderEvents(){
+  renderEvents() {
     const allevents = this.state.EventArray;
 
     const filteredEvents = this.state.EventArray.filter((event) => {
-        return Object.values(event).join("").toLowerCase().includes(this.state.searchVal.toLowerCase());
-        
+      return Object.values(event)
+        .join("")
+        .toLowerCase()
+        .includes(this.state.searchVal.toLowerCase());
     });
 
     return filteredEvents.map((currEvent) => (
-        <div>
-          <a href="#" onClick={(e) => {e.preventDefault(); console.log("clicked"); this.handleClick(currEvent)}} >
-            <Event key={currEvent._id} eventData={currEvent} />
-          </a>
-        </div>
-          
-
-      
-    ))
+      <div>
+        <a
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            console.log("clicked");
+            this.handleClick(currEvent);
+          }}
+        >
+          <Event key={currEvent._id} eventData={currEvent} />
+        </a>
+      </div>
+    ));
   }
 
-
-
- 
-
-
   render() {
-    
     return (
-      
       <section id="resume">
-        <Slide left duration={1300}>
         {/* {this.state.isModalOpen && <Modal onRequestClose={this.toggleModal} ModalData={this.state.ModalData} />} */}
-        
+
         {this.state.EventPg === true ? (
-          <Modal currEvent={this.state.CurrentEventData} handleClose={this.handleClose} />
-        ):
-        (
+          <Modal
+            currEvent={this.state.CurrentEventData}
+            handleClose={this.handleClose}
+          />
+        ) : (
           <div>
             <div className="row education">
               <div className="twelve columns header-col">
                 <h1>
-                  <span style={{color:"white"}}>Events</span>
+                  <span style={{ color: "white" }}>Events</span>
                 </h1>
                 <form id="searchBar">
-                  <input ref={this.searchBoxRef} type="search" 
-                  placeholder="Search" 
-                  defaultValue={this.state.searchVal} 
-                  onChange={this.handleSearch}/>
+                  <input
+                    ref={this.searchBoxRef}
+                    type="search"
+                    placeholder="Search"
+                    defaultValue={this.state.searchVal}
+                    onChange={this.handleSearch}
+                  />
                 </form>
-
               </div>
 
               <div className="eleven columns main-col">
-                <div className="cards-list">
-                  {this.renderEvents()}
-                </div>
+                <div className="cards-list">{this.renderEvents()}</div>
               </div>
             </div>
-          
-        </div>
-        )
-        }
-        </Slide>
+          </div>
+        )}
       </section>
     );
   }
 }
 
 export default Resume;
-
-
 
 /* <Slide left duration={1300}>
           <div className="row work">
